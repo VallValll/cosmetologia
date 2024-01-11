266694064f91df72899c096bfc9c616b731e4974
VallValll <afanasieva_vv@mail.ru>
2024-01-11 12:11:17 +0300

    add block specialist

---
+++ js/main.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const accordionHeaders = document.querySelectorAll('.js-accordion');
+  
+    accordionHeaders.forEach(el => {
+      el.querySelector('.accordion__header').addEventListener('click', function() {
+        const accordionItem = this.parentElement;
+        console.log(accordionItem)
+  
+        accordionItem.classList.toggle('show');
+      });
+    });
+});
+
